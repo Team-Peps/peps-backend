afba45c4360a1bb30aaf1e92500af91acc1b4603
augustinde <augustin.1201@outlook.fr>
2025-02-22 16:36:12 +0100

    initial commit

---
+++ src/main/java/fr/teampeps/PepsBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.teampeps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PepsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/teampeps/PepsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.teampeps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
